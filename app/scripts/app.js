060a013e364527a2c9702d3c358bdb45a15565be
amizony <amizony@gmail.com>
2015-06-03 11:37:13 +0200

    Monitoring and hidding old tasks

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,7 +22,7 @@ taskList.controller("MainController.controller", ["$scope", "$firebaseArray", fu
 
   var ref = new Firebase("https://luminous-fire-9311.firebaseio.com/messages");
   $scope.tasks = $firebaseArray(ref);
-
+  var intervalID;
 
   $scope.addTask = function() {
     var time = new Date();
@@ -33,6 +33,22 @@ taskList.controller("MainController.controller", ["$scope", "$firebaseArray", fu
       priority: "medium"
     });
     $scope.newTaskDescription = "";
+  };
+
+  var clearOldTasks = function() {
+    var time = new Date();
+    for (var i = 0; i < $scope.tasks.length; i++) {
+      var age = time.getTime() - $scope.tasks[i].date;
+      if (age > 15000) {
+        $scope.tasks[i].status = "expired";
+      }
+    }
+  };
+
+  $scope.watchForOldTasks = function() {
+    if (!intervalID) {
+      intervalID = setInterval(clearOldTasks, 10000);
+    }
   };
 
 }]);
